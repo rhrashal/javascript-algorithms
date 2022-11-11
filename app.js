76e27d18bfbc2d5ea874fd193d4b78004f9059fd
rhrashal <rhrashal@gmail.com>
2022-11-11 11:59:01 +0600

    .

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let array = [2,5,9,12,6,9,8];
+let array = [2,5,9,12,6,8];
 
+let position =3;
 
+for (let i = position; i < array.length-1; i++) {
+    array[i]=array[i+1]
+    
+}
 
-
+array.length = array.length-1;
 console.log(array);
