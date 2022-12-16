c4a1b79af4447c14be2a55924288029cfb72e0b9
rhrashal <rhrashal@gmail.com>
2022-12-17 00:37:26 +0600

    .

--- Binary_Search.js
+++ Binary_Search.js
@@ -1,5 +1,5 @@
 let data = [20,21,23,45,76,90,98];
-let find =90;
+let find =23;
 let start =0;
 let end = data.length-1;
 let position=undefined;
@@ -26,20 +26,23 @@ function recoursiveBinary(data,start,end){
 recoursiveBinary(data,start,end);
 
 
+
+
+
 //Iterative approach
 
-// while (start<=end) {
-//    let mid=Math.floor((start+end)/2);
-//    //console.log(mid);
-//    if (data[mid]===find) {
-//       position=mid;
-//       break;
-//    }else if (data[mid]<find) {
-//       start=mid+1;
-//    }else{
-//       end=mid-1;
-//    }
+while (start<=end) {
+   let mid=Math.floor((start+end)/2);
+   //console.log(mid);
+   if (data[mid]===find) {
+      position=mid;
+      break;
+   }else if (data[mid]<find) {
+      start=mid+1;
+   }else{
+      end=mid-1;
+   }
 
-// }
+}
 
 console.log(position);
